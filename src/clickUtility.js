26547f7a052a7e3836158167bbe9aaa584f2d38e
Kaiyu Hsu <im.kaiyu@gmail.com>
2015-04-20 22:28:04 -0400

    Added click handlers to Element prototype

--- src/clickUtility.js
+++ src/clickUtility.js
@@ -1,17 +1,17 @@
 (function(){
   'use strict';
 
-  HTMLElement.prototype.click = function(callback) {
+  BeltElement.prototype.click = function(callback) {
     this.addEventListener("click", callback);
     return this;
   };
 
-  HTMLElement.prototype.mouseOver = function(callback) {
+  BeltElement.prototype.mouseOver = function(callback) {
     this.addEventListener("mouseover", callback);
     return this;
   };
 
-  HTMLElement.prototype.mouseOut = function(callback) {
+  BeltElement.prototype.mouseOut = function(callback) {
     this.addEventListener("mouseout", callback);
     return this;
   };
